a595bc2293510676f9765956a96df611c29426cd
Károly P <tinywall@pados.hu>
2012-02-13 10:11:04 +0100

    Attempt at fix: Do not lose settings under a virtual machine.

--- TinyWall/MachineFingerprint.cs
+++ TinyWall/MachineFingerprint.cs
@@ -10,129 +10,34 @@ namespace PKSoft
         {
             if (string.IsNullOrEmpty(fingerPrint))
             {
-                fingerPrint = Utils.GetHash("CPU >> " + cpuId() + "\nBIOS >> " + biosId() + "\nBASE >> " + baseId()
-                            //+"\nDISK >> "+ diskId() + "\nVIDEO >> " + videoId() +"\nMAC >> "+ macId()
-                );
+                fingerPrint = Utils.GetHash(
+                    identifier("SerialNumber", "Win32_OperatingSystem") +
+                    identifier("Product", "Win32_BaseBoard")
+                    );
             }
             return fingerPrint;
         }
 
-        #region Original Device ID Getting Code
-        //Return a hardware identifier
-        private static string identifier(string wmiClass, string wmiProperty, string wmiMustBeTrue)
+        private static string identifier(string wmiProperty, string wmiClass)
         {
             string result = "";
-            using (System.Management.ManagementClass mc = new System.Management.ManagementClass(wmiClass))
+            using (ManagementObjectSearcher mc = new ManagementObjectSearcher(string.Format(@"select {0} from {1}", wmiProperty, wmiClass)))
             {
-                System.Management.ManagementObjectCollection moc = mc.GetInstances();
-                foreach (System.Management.ManagementObject mo in moc)
+                ManagementObjectCollection moc = mc.Get();
+                foreach (ManagementObject mo in moc)
                 {
-                    if (mo[wmiMustBeTrue].ToString() == "True")
+                    try
                     {
-                        //Only get the first one
-                        if (string.IsNullOrEmpty(result))
-                        {
-                            try
-                            {
-                                result = mo[wmiProperty].ToString();
-                                break;
-                            }
-                            catch
-                            {
-                            }
-                        }
-                    }
-                }
-            }
-            return result;
-        }
-        //Return a hardware identifier
-        private static string identifier(string wmiClass, string wmiProperty)
-        {
-            string result = "";
-            using (System.Management.ManagementClass mc = new System.Management.ManagementClass(wmiClass))
-            {
-                System.Management.ManagementObjectCollection moc = mc.GetInstances();
-                foreach (System.Management.ManagementObject mo in moc)
-                {
-                    //Only get the first one
-                    if (string.IsNullOrEmpty(result))
-                    {
-                        try
-                        {
-                            result = mo[wmiProperty].ToString();
+                        result = mo[wmiProperty].ToString();
+                        if (!string.IsNullOrEmpty(result))
                             break;
-                        }
-                        catch
-                        {
-                        }
+                    }
+                    catch
+                    {
                     }
                 }
             }
             return result;
         }
-        private static string cpuId()
-        {
-            //Uses first CPU identifier available in order of preference
-            //Don't get all identifiers, as it is very time consuming
-            string retVal = identifier("Win32_Processor", "UniqueId");
-            if (string.IsNullOrEmpty(retVal)) //If no UniqueID, use ProcessorID
-            {
-                retVal = identifier("Win32_Processor", "ProcessorId");
-                if (string.IsNullOrEmpty(retVal)) //If no ProcessorId, use Name
-                {
-                    retVal = identifier("Win32_Processor", "Name");
-                    if (string.IsNullOrEmpty(retVal)) //If no Name, use Manufacturer
-                    {
-                        retVal = identifier("Win32_Processor", "Manufacturer");
-                    }
-                    //Add clock speed for extra security
-                    retVal += identifier("Win32_Processor", "MaxClockSpeed");
-                }
-            }
-            return retVal;
-        }
-        //BIOS Identifier
-        private static string biosId()
-        {
-            return identifier("Win32_BIOS", "Manufacturer")
-            + identifier("Win32_BIOS", "SMBIOSBIOSVersion")
-            + identifier("Win32_BIOS", "IdentificationCode")
-            + identifier("Win32_BIOS", "SerialNumber")
-            + identifier("Win32_BIOS", "ReleaseDate")
-            + identifier("Win32_BIOS", "Version");
-        }
-        //Motherboard ID
-        private static string baseId()
-        {
-            return identifier("Win32_BaseBoard", "Model")
-            + identifier("Win32_BaseBoard", "Manufacturer")
-            + identifier("Win32_BaseBoard", "Name")
-            + identifier("Win32_BaseBoard", "SerialNumber");
-        }
-
-        /*
-        //Primary video controller ID
-        private static string videoId()
-        {
-            return identifier("Win32_VideoController", "DriverVersion")
-            + identifier("Win32_VideoController", "Name");
-        }
-        //First enabled network card ID
-        private static string macId()
-        {
-            return identifier("Win32_NetworkAdapterConfiguration",
-                "MACAddress", "IPEnabled");
-        }
-        //Main physical hard drive ID
-        private static string diskId()
-        {
-            return identifier("Win32_DiskDrive", "Model")
-            + identifier("Win32_DiskDrive", "Manufacturer")
-            + identifier("Win32_DiskDrive", "Signature")
-            + identifier("Win32_DiskDrive", "TotalHeads");
-        }
-         */
-        #endregion
     }
 }
