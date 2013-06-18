352aeb14deb62003b3c6f45c556d84943afb7f52
Károly P <tinywall@pados.hu>
2013-06-18 20:22:10 +0200

    Improvements and new feature to develtool: - End-user warn message - ResX optimizter - Batch signtool signer

--- TinyWall/DevelToolForm.Designer.cs
+++ TinyWall/DevelToolForm.Designer.cs
@@ -65,11 +65,36 @@
             this.btnUpdateCreate = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.listPrimaries = new System.Windows.Forms.ListBox();
+            this.txtOutputPath = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnAddPrimaries = new System.Windows.Forms.Button();
+            this.btnResxClear = new System.Windows.Forms.Button();
+            this.btnOptimize = new System.Windows.Forms.Button();
+            this.listSatellites = new System.Windows.Forms.ListBox();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.txtTimestampingServ = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.btnSigntoolBrowse = new System.Windows.Forms.Button();
+            this.txtSigntool = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.btnBatchSign = new System.Windows.Forms.Button();
+            this.btnSignDir = new System.Windows.Forms.Button();
+            this.txtSignDir = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.txtCertPass = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnCertBrowse = new System.Windows.Forms.Button();
+            this.txtCert = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.tabPage4.SuspendLayout();
             this.tabPage3.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.tabPage6.SuspendLayout();
             this.SuspendLayout();
             // 
             // ofd
@@ -173,6 +198,8 @@
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -423,6 +450,234 @@
             this.tabPage3.Text = "Strong name";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.listPrimaries);
+            this.tabPage5.Controls.Add(this.txtOutputPath);
+            this.tabPage5.Controls.Add(this.label9);
+            this.tabPage5.Controls.Add(this.btnAddPrimaries);
+            this.tabPage5.Controls.Add(this.btnResxClear);
+            this.tabPage5.Controls.Add(this.btnOptimize);
+            this.tabPage5.Controls.Add(this.listSatellites);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage5.Size = new System.Drawing.Size(764, 323);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "ResX optimizer";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // listPrimaries
+            // 
+            this.listPrimaries.FormattingEnabled = true;
+            this.listPrimaries.Location = new System.Drawing.Point(6, 6);
+            this.listPrimaries.Name = "listPrimaries";
+            this.listPrimaries.Size = new System.Drawing.Size(212, 264);
+            this.listPrimaries.TabIndex = 8;
+            // 
+            // txtOutputPath
+            // 
+            this.txtOutputPath.Location = new System.Drawing.Point(9, 289);
+            this.txtOutputPath.Name = "txtOutputPath";
+            this.txtOutputPath.Size = new System.Drawing.Size(349, 20);
+            this.txtOutputPath.TabIndex = 12;
+            this.txtOutputPath.Text = "D:\\Projects\\TinyWall\\Temp";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 273);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Output path:";
+            // 
+            // btnAddPrimaries
+            // 
+            this.btnAddPrimaries.Location = new System.Drawing.Point(224, 19);
+            this.btnAddPrimaries.Name = "btnAddPrimaries";
+            this.btnAddPrimaries.Size = new System.Drawing.Size(131, 30);
+            this.btnAddPrimaries.TabIndex = 7;
+            this.btnAddPrimaries.Text = "Add Primaries";
+            this.btnAddPrimaries.UseVisualStyleBackColor = true;
+            this.btnAddPrimaries.Click += new System.EventHandler(this.btnAddPrimaries_Click);
+            // 
+            // btnResxClear
+            // 
+            this.btnResxClear.Location = new System.Drawing.Point(224, 55);
+            this.btnResxClear.Name = "btnResxClear";
+            this.btnResxClear.Size = new System.Drawing.Size(131, 30);
+            this.btnResxClear.TabIndex = 11;
+            this.btnResxClear.Text = "Clear";
+            this.btnResxClear.UseVisualStyleBackColor = true;
+            this.btnResxClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnOptimize
+            // 
+            this.btnOptimize.Location = new System.Drawing.Point(224, 115);
+            this.btnOptimize.Name = "btnOptimize";
+            this.btnOptimize.Size = new System.Drawing.Size(131, 30);
+            this.btnOptimize.TabIndex = 10;
+            this.btnOptimize.Text = "Optimize!";
+            this.btnOptimize.UseVisualStyleBackColor = true;
+            this.btnOptimize.Click += new System.EventHandler(this.btnOptimize_Click);
+            // 
+            // listSatellites
+            // 
+            this.listSatellites.FormattingEnabled = true;
+            this.listSatellites.Location = new System.Drawing.Point(361, 6);
+            this.listSatellites.Name = "listSatellites";
+            this.listSatellites.Size = new System.Drawing.Size(212, 264);
+            this.listSatellites.TabIndex = 9;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.txtTimestampingServ);
+            this.tabPage6.Controls.Add(this.label14);
+            this.tabPage6.Controls.Add(this.btnSigntoolBrowse);
+            this.tabPage6.Controls.Add(this.txtSigntool);
+            this.tabPage6.Controls.Add(this.label13);
+            this.tabPage6.Controls.Add(this.btnBatchSign);
+            this.tabPage6.Controls.Add(this.btnSignDir);
+            this.tabPage6.Controls.Add(this.txtSignDir);
+            this.tabPage6.Controls.Add(this.label12);
+            this.tabPage6.Controls.Add(this.txtCertPass);
+            this.tabPage6.Controls.Add(this.label11);
+            this.tabPage6.Controls.Add(this.btnCertBrowse);
+            this.tabPage6.Controls.Add(this.txtCert);
+            this.tabPage6.Controls.Add(this.label10);
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage6.Size = new System.Drawing.Size(764, 323);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Batch signer";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // txtTimestampingServ
+            // 
+            this.txtTimestampingServ.Location = new System.Drawing.Point(9, 199);
+            this.txtTimestampingServ.Name = "txtTimestampingServ";
+            this.txtTimestampingServ.Size = new System.Drawing.Size(329, 20);
+            this.txtTimestampingServ.TabIndex = 13;
+            this.txtTimestampingServ.Text = "http://www.startssl.com/timestamp";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(6, 183);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(112, 13);
+            this.label14.TabIndex = 12;
+            this.label14.Text = "Timestamping service:";
+            // 
+            // btnSigntoolBrowse
+            // 
+            this.btnSigntoolBrowse.Location = new System.Drawing.Point(344, 158);
+            this.btnSigntoolBrowse.Name = "btnSigntoolBrowse";
+            this.btnSigntoolBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnSigntoolBrowse.TabIndex = 11;
+            this.btnSigntoolBrowse.Text = "Browse...";
+            this.btnSigntoolBrowse.UseVisualStyleBackColor = true;
+            this.btnSigntoolBrowse.Click += new System.EventHandler(this.btnSigntoolBrowse_Click);
+            // 
+            // txtSigntool
+            // 
+            this.txtSigntool.Location = new System.Drawing.Point(9, 160);
+            this.txtSigntool.Name = "txtSigntool";
+            this.txtSigntool.Size = new System.Drawing.Size(329, 20);
+            this.txtSigntool.TabIndex = 10;
+            this.txtSigntool.Text = "C:\\Program Files (x86)\\Microsoft SDKs\\Windows\\v7.1A\\Bin\\Signtool.exe";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(6, 144);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(88, 13);
+            this.label13.TabIndex = 9;
+            this.label13.Text = "Signtool location:";
+            // 
+            // btnBatchSign
+            // 
+            this.btnBatchSign.Location = new System.Drawing.Point(9, 235);
+            this.btnBatchSign.Name = "btnBatchSign";
+            this.btnBatchSign.Size = new System.Drawing.Size(109, 59);
+            this.btnBatchSign.TabIndex = 8;
+            this.btnBatchSign.Text = "Batch sign!";
+            this.btnBatchSign.UseVisualStyleBackColor = true;
+            this.btnBatchSign.Click += new System.EventHandler(this.btnBatchSign_Click);
+            // 
+            // btnSignDir
+            // 
+            this.btnSignDir.Location = new System.Drawing.Point(344, 119);
+            this.btnSignDir.Name = "btnSignDir";
+            this.btnSignDir.Size = new System.Drawing.Size(75, 23);
+            this.btnSignDir.TabIndex = 7;
+            this.btnSignDir.Text = "Browse...";
+            this.btnSignDir.UseVisualStyleBackColor = true;
+            this.btnSignDir.Click += new System.EventHandler(this.btnSignDir_Click);
+            // 
+            // txtSignDir
+            // 
+            this.txtSignDir.Location = new System.Drawing.Point(9, 121);
+            this.txtSignDir.Name = "txtSignDir";
+            this.txtSignDir.Size = new System.Drawing.Size(329, 20);
+            this.txtSignDir.TabIndex = 6;
+            this.txtSignDir.Text = "D:\\Projects\\TinyWall\\MsiSetup\\";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(6, 105);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(118, 13);
+            this.label12.TabIndex = 5;
+            this.label12.Text = "Directory to sign files in:";
+            // 
+            // txtCertPass
+            // 
+            this.txtCertPass.Location = new System.Drawing.Point(9, 82);
+            this.txtCertPass.Name = "txtCertPass";
+            this.txtCertPass.Size = new System.Drawing.Size(329, 20);
+            this.txtCertPass.TabIndex = 4;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(6, 66);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(105, 13);
+            this.label11.TabIndex = 3;
+            this.label11.Text = "Certificate password:";
+            // 
+            // btnCertBrowse
+            // 
+            this.btnCertBrowse.Location = new System.Drawing.Point(344, 41);
+            this.btnCertBrowse.Name = "btnCertBrowse";
+            this.btnCertBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnCertBrowse.TabIndex = 2;
+            this.btnCertBrowse.Text = "Browse...";
+            this.btnCertBrowse.UseVisualStyleBackColor = true;
+            this.btnCertBrowse.Click += new System.EventHandler(this.btnCertBrowse_Click);
+            // 
+            // txtCert
+            // 
+            this.txtCert.Location = new System.Drawing.Point(9, 43);
+            this.txtCert.Name = "txtCert";
+            this.txtCert.Size = new System.Drawing.Size(329, 20);
+            this.txtCert.TabIndex = 1;
+            this.txtCert.Text = "D:\\Projects\\TinyWall\\MsiSetup\\";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(6, 27);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(73, 13);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Certificate file:";
+            // 
             // DevelToolForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -444,6 +699,10 @@
             this.tabPage4.PerformLayout();
             this.tabPage3.ResumeLayout(false);
             this.tabPage3.PerformLayout();
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage5.PerformLayout();
+            this.tabPage6.ResumeLayout(false);
+            this.tabPage6.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -487,5 +746,28 @@
         private System.Windows.Forms.TextBox txtUpdateURL;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Button btnUpdateInstallerBrowse;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.ListBox listPrimaries;
+        private System.Windows.Forms.TextBox txtOutputPath;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnAddPrimaries;
+        private System.Windows.Forms.Button btnResxClear;
+        private System.Windows.Forms.Button btnOptimize;
+        private System.Windows.Forms.ListBox listSatellites;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.Button btnBatchSign;
+        private System.Windows.Forms.Button btnSignDir;
+        private System.Windows.Forms.TextBox txtSignDir;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox txtCertPass;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnCertBrowse;
+        private System.Windows.Forms.TextBox txtCert;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button btnSigntoolBrowse;
+        private System.Windows.Forms.TextBox txtSigntool;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox txtTimestampingServ;
+        private System.Windows.Forms.Label label14;
     }
 }
--- TinyWall/DevelToolForm.cs
+++ TinyWall/DevelToolForm.cs
@@ -1,20 +1,38 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.IO;
+using System.Resources;
 using System.Reflection;
+using System.Text;
 using System.Windows.Forms;
 
 namespace PKSoft
 {
     internal partial class DevelToolForm : Form
     {
+        // Key - The primary resource
+        // Value - List of satellite resources
+        private List<KeyValuePair<string, string[]>> ResXInputs = new List<KeyValuePair<string, string[]>>();
+
         internal DevelToolForm()
         {
+            System.Windows.Forms.MessageBox.Show(
+                "This tool is not meant for end-users. Only use this tool when instructed to do so by the application developer.",
+                "Warning: Not for users!",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Exclamation
+                );
+
             InitializeComponent();
         }
 
         private void btnAssocBrowse_Click(object sender, EventArgs e)
         {
+            ofd.Filter = "All files (*)|*";
             if (ofd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
             {
                 txtAssocExePath.Text = ofd.FileName;
@@ -89,6 +107,7 @@ namespace PKSoft
 
         private void btnStrongNameBrowse_Click(object sender, EventArgs e)
         {
+            ofd.Filter = ".Net binaries (*.exe,*.dll)|*.dll;*.exe|All files (*)|*";
             if (ofd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
             {
                 try
@@ -118,6 +137,7 @@ namespace PKSoft
 
         private void btnUpdateInstallerBrowse_Click(object sender, EventArgs e)
         {
+            ofd.Filter = "All files (*)|*";
             if (ofd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
             {
                 txtUpdateTWInstaller.Text = ofd.FileName;
@@ -126,6 +146,7 @@ namespace PKSoft
         
         private void btnUpdateDatabaseBrowse_Click(object sender, EventArgs e)
         {
+            ofd.Filter = "All files (*)|*";
             if (ofd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
             {
                 txtUpdateDatabase.Text = ofd.FileName;
@@ -134,6 +155,7 @@ namespace PKSoft
 
         private void button1_Click(object sender, EventArgs e)
         {
+            ofd.Filter = "All files (*)|*";
             if (ofd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
             {
                 txtUpdateHosts.Text = ofd.FileName;
@@ -198,6 +220,214 @@ namespace PKSoft
             string updOut = Path.Combine(txtUpdateOutput.Text, XML_OUT_NAME);
             SerializationHelper.SaveToXMLFile(update, updOut);
             MessageBox.Show(this, "Update created.", "Success.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnAddPrimaries_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "XML resources (*.resx)|*.resx|All files (*)|*";
+            ofd.AutoUpgradeEnabled = true;
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog(this) == System.Windows.Forms.DialogResult.Cancel)
+                return;
+
+            for (int i = 0; i < ofd.FileNames.Length; ++i)
+            {
+                string primary = ofd.FileNames[i];
+                string dir = Path.GetDirectoryName(primary);
+                string primaryBase = Path.GetFileNameWithoutExtension(primary);
+                string primaryBasePath = Path.Combine(dir, primaryBase);
+                string[] satellites = Directory.GetFiles(dir, primaryBase + ".*.resx", SearchOption.TopDirectoryOnly);
+                ResXInputs.Add(new KeyValuePair<string, string[]>(primary, satellites));
+            }
+
+            listPrimaries.Items.Clear();
+            for (int i = 0; i < ResXInputs.Count; ++i)
+                listPrimaries.Items.Add(Path.GetFileName(ResXInputs[i].Key));
+        }
+
+        private void listPrimaries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listSatellites.Items.Clear();
+            if (listPrimaries.SelectedIndices.Count > 0)
+            {
+                KeyValuePair<string, string[]> pair = ResXInputs[listPrimaries.SelectedIndex];
+                object[] sats = new object[pair.Value.Length];
+                for (int i = 0; i < sats.Length; ++i)
+                    sats[i] = Path.GetFileName(pair.Value[i]);
+                listSatellites.Items.AddRange(sats);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listPrimaries.Items.Clear();
+            listSatellites.Items.Clear();
+            ResXInputs.Clear();
+        }
+
+        private static Dictionary<string, ResXDataNode> ReadResXFile(string filePath)
+        {
+            Dictionary<string, ResXDataNode> resxContents = new Dictionary<string, ResXDataNode>();
+            using (ResXResourceReader resxReader = new ResXResourceReader(filePath))
+            {
+                resxReader.UseResXDataNodes = true;
+                IDictionaryEnumerator dict = resxReader.GetEnumerator();
+                while (dict.MoveNext())
+                {
+                    ResXDataNode node = (ResXDataNode)dict.Value;
+                    resxContents.Add(node.Name, node);
+                }
+            }
+
+            return resxContents;
+        }
+
+        private void btnOptimize_Click(object sender, EventArgs e)
+        {
+            ITypeResolutionService trs = null;
+
+            for (int i = 0; i < ResXInputs.Count; ++i)  // for each main resource file
+            {
+                KeyValuePair<string, string[]> pair = ResXInputs[i];
+
+                Dictionary<string, ResXDataNode> primary = ReadResXFile(pair.Key);
+
+                for (int s = 0; s < pair.Value.Length; ++s)  // for each localization
+                {
+                    { // Replace Windows Forms control versions to 2.0.0.0.
+                        // (They were often rewritten to 4.0.0.0 wrongly during localization)
+                        string a = null;
+                        using (StreamReader sr = new StreamReader(pair.Value[s], Encoding.UTF8))
+                        {
+                            a = sr.ReadToEnd();
+                        }
+                        a = a.Replace(", Version=4.0.0.0,", ", Version=2.0.0.0,");
+                        using (StreamWriter sw = new StreamWriter(pair.Value[s], false, Encoding.UTF8))
+                        {
+                            sw.Write(a);
+                        }
+                    }
+
+                    Dictionary<string, ResXDataNode> satellite = ReadResXFile(pair.Value[s]);
+                    Dictionary<string, ResXDataNode> newSatellite = new Dictionary<string, ResXDataNode>();
+
+                    // Iterate over all contents of primary.
+                    // For each entry, check if one with same name, type and contents is available in
+                    // satellite, and if so, don't save it to output.
+                    Dictionary<string, ResXDataNode>.Enumerator primaryEnum = primary.GetEnumerator();
+                    while (primaryEnum.MoveNext())
+                    {
+                        ResXDataNode primaryItem = primaryEnum.Current.Value;
+                        if (!satellite.ContainsKey(primaryItem.Name))
+                            continue;
+
+                        ResXDataNode satelliteItem = satellite[primaryItem.Name];
+
+                        // Only save localized resource if it is different from the default 
+                        if (!satelliteItem.GetValue(trs).Equals(primaryItem.GetValue(trs)))
+                            newSatellite.Add(satelliteItem.Name, satelliteItem);
+                        else
+                        {
+                        }
+                    }
+
+                    // Write output ResX file
+                    string outPath = Path.Combine(txtOutputPath.Text, Path.GetFileName(pair.Value[s]));
+                    using (ResXResourceWriter resxWriter = new ResXResourceWriter(outPath))
+                    {
+                        Dictionary<string, ResXDataNode>.Enumerator outputEnum = newSatellite.GetEnumerator();
+                        while (outputEnum.MoveNext())
+                            resxWriter.AddResource(outputEnum.Current.Value);
+                        resxWriter.Generate();
+                    }
+                } // for each localization
+            } // for each primary
+        }
+
+        private void btnCertBrowse_Click(object sender, EventArgs e)
+        {
+            ofd.InitialDirectory = Path.GetDirectoryName(txtCert.Text);
+            ofd.Filter = "All files (*)|*";
+            if (ofd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+            {
+                txtCert.Text = ofd.FileName;
+            }
+        }
+
+        private void btnSignDir_Click(object sender, EventArgs e)
+        {
+            fbd.SelectedPath = Path.GetDirectoryName(txtCert.Text);
+            if (fbd.ShowDialog(this) == System.Windows.Forms.DialogResult.Cancel)
+                return;
+
+            txtSignDir.Text = fbd.SelectedPath;
+        }
+
+        private void btnBatchSign_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(txtCert.Text))
+            {
+                MessageBox.Show(this, "Certificate not found!");
+                return;
+            }
+            if (!Directory.Exists(txtSignDir.Text))
+            {
+                MessageBox.Show(this, "Signing directory is invalid!");
+                return;
+            }
+            if (!File.Exists(txtSigntool.Text))
+            {
+                MessageBox.Show(this, "Signtool.exe not found!");
+                return;
+            }
+
+            SignFiles(txtSignDir.Text, "*.dll");
+            SignFiles(txtSignDir.Text, "*.exe");
+            SignFiles(txtSignDir.Text, "*.msi");
+            MessageBox.Show(this, "Done signing!");
+        }
+
+        private void SignFiles(string dirPath, string filePattern)
+        {
+            string[] files = Directory.GetFiles(dirPath, filePattern, SearchOption.AllDirectories);
+            for (int i = 0; i < files.Length; ++i)
+            {
+                string signParams = string.Format("sign /f \"{0}\" /p \"{1}\" /d TinyWall /du \"http://tinywall.pados.hu\" /tr \"{2}\" \"{3}\"",
+                        txtCert.Text,
+                        txtCertPass.Text,
+                        txtTimestampingServ.Text,
+                        files[i]);
+
+                // Because signing accesses the timestamping server over the web,
+                // we retry a failed signing multiple times to account for
+                // internet glitches.
+                bool signed = false;
+                for (int retry = 0; retry < 3; ++retry)
+                {
+                    using (Process p = Utils.StartProcess(txtSigntool.Text, signParams, false))
+                    {
+                        p.WaitForExit();
+                        signed = signed || (p.ExitCode == 0);
+                    }
+
+                    if (signed)
+                        break;
+
+                    System.Threading.Thread.Sleep(1000);
+                }
+                if (!signed)
+                    MessageBox.Show(this, "Failed to sign: " + files[i]);
+            }
+        }
+
+        private void btnSigntoolBrowse_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "Executables (*.exe)|*.exe|All files (*)|*";
+            if (ofd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+            {
+                txtSigntool.Text = ofd.FileName;
+            }
+
         }
     }
 }
