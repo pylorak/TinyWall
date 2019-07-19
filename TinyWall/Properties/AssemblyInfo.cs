ec713ad34e1705fd090bde46d855872e05b105d8
Károly P <tinywall@pados.hu>
2019-07-19 03:50:28 +0200

    Finalize 2.1.10 release.

--- TinyWall/Properties/AssemblyInfo.cs
+++ TinyWall/Properties/AssemblyInfo.cs
@@ -33,9 +33,9 @@ using System.Resources;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.9")]                  // used by the CLR
-[assembly: AssemblyFileVersion("2.1.9")]              // full assembly version
-[assembly: AssemblyInformationalVersion("2.1.9")]     // informal version for customers
+[assembly: AssemblyVersion("2.1.10")]                  // used by the CLR
+[assembly: AssemblyFileVersion("2.1.10")]              // full assembly version
+[assembly: AssemblyInformationalVersion("2.1.10")]     // informal version for customers
 [assembly: NeutralResourcesLanguageAttribute("en")]
 
 [assembly: CLSCompliant(true)]
