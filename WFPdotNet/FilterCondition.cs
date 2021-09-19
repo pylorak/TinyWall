0d45adc8cd2af02276ec56194471c95a0f2ea9ff
Károly P <tinywall@pados.hu>
2021-09-19 14:22:21 +0200

    "System" AppId is case-sensitive in WFP.

--- WFPdotNet/FilterCondition.cs
+++ WFPdotNet/FilterCondition.cs
@@ -354,7 +354,7 @@ namespace WFPdotNet
             if (bBeforeProxying && !VersionInfo.Win8OrNewer)
                 throw new NotSupportedException("FWPM_CONDITION_ALE_ORIGINAL_APP_ID (set by bBeforeProxying) requires Windows 8 or newer.");
 
-            if (!alreadyKernelFormat && (System.IO.File.Exists(filePath) || filePath.Equals("System", StringComparison.OrdinalIgnoreCase)))
+            if ((!alreadyKernelFormat && (System.IO.File.Exists(filePath)) || filePath.Equals("System", StringComparison.OrdinalIgnoreCase)))
             {
                 // NOTE: FwpmGetAppIdFromFileName0() will sometimes create invalid AppIds depending on
                 //       international characters and casing. To avoid this, it is recommended to
