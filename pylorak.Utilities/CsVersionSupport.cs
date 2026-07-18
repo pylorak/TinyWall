711335deafd639d3a94aa8d0dd638c35a4db5ef7
Károly P. <tinywall@pados.hu>
2026-07-18 04:51:40 +0200

    Upgrade C# language version to 11.0.

---
+++ pylorak.Utilities/CsVersionSupport.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+// This file contains polyfills to enable compile features on older .Net versions
+
+#region Support for init keyword, natively supported only in .Net 5+
+#if !NET
+namespace System.Runtime.CompilerServices
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static class IsExternalInit
+    {
+    }
+}
+#endif
+#endregion
+
+#region Support for required keyword, natively supported only in .Net 7+
+#if !NET7_0_OR_GREATER
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public sealed class RequiredMemberAttribute : Attribute
+    { }
+
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = false)]
+    public sealed class CompilerFeatureRequiredAttribute : Attribute
+    {
+        public CompilerFeatureRequiredAttribute(string featureName)
+        {
+            FeatureName = featureName;
+        }
+
+        public string FeatureName { get; }
+        public bool IsOptional { get; init; }
+        public const string RefStructs = nameof(RefStructs);
+        public const string RequiredMembers = nameof(RequiredMembers);
+    }
+}
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Constructor, AllowMultiple = false, Inherited = false)]
+    public sealed class SetsRequiredMembersAttribute : Attribute
+    {
+    }
+}
+#endif
+#endregion
